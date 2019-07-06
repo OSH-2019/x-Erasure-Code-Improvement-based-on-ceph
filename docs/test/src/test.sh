4d5ae56bdc593d0c112d2b4c9308eff8b995e60e
ZhangMZh <35245096+ZhangMZh@users.noreply.github.com>
2019-07-06 20:57:08 +0800

    Update test.sh

--- docs/test/src/test.sh
+++ docs/test/src/test.sh
@@ -5,7 +5,7 @@
 ## test SIMD
 
 sudo rados bench -p isa 600 write --no-cleanup
-sudo rados benxh -p isa 600 seq
+sudo rados bench -p isa 600 seq
 sudo rados bench -p isa 600 rand
 sudo rados -p isa cleanup
 
